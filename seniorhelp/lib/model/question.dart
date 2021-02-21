b4233502a581069892b2f3e2a46b43f3dc22d523
samiraliyev573 <aliyev7137@gmail.com>
2021-02-21 02:45:37 -0800

    Quiz Page init
    Co-Authored-By: Prachi Gandhi <78549313+prachigandhi001@users.noreply.github.com>

---
+++ seniorhelp/lib/model/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String question;
+
+  String option1;
+  String option2;
+  String option3;
+  String option4;
+  String answer;
+  Question(this.question, this.option1, this.option2, this.option3,
+      this.option4, this.answer);
+}
