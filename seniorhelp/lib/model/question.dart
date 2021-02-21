9f02847b771103f1646300f8f5d115f670e92b10
samiraliyev573 <aliyev7137@gmail.com>
2021-02-21 03:06:34 -0800

    Adding Questions for the Quiz Page
    Amaan Alikhan - Created all the necessary information for the quiz
    Prachi - Developed simple question model that is in array.
    
    Co-Authored-By: Prachi Gandhi <78549313+prachigandhi001@users.noreply.github.com>
    Co-Authored-By: Amaan Ali Khan <78450861+AmaanAliK@users.noreply.github.com>

--- seniorhelp/lib/model/question.dart
+++ seniorhelp/lib/model/question.dart
@@ -1,11 +1,67 @@
 class Question {
   String question;
-
-  String option1;
-  String option2;
-  String option3;
-  String option4;
+  List<String> options;
   String answer;
-  Question(this.question, this.option1, this.option2, this.option3,
-      this.option4, this.answer);
+  Question(this.question, this.options, this.answer);
 }
+
+final quiz = [
+  Question(
+    "What are your children’s names?",
+    [
+      "Jack and Jill Doe",
+      "Jayden and Jane Doe",
+      "James and Jen Doe) ",
+      "Joe and June Doe"
+    ],
+    "Jack and Jill Doe",
+  ),
+  Question(
+    "How old are your children?",
+    ["39 and 36", "45 and 40", "34 and 39", "44 and 55"],
+    "39 and 36",
+  ),
+  Question(
+    "What was your nickname growing up?",
+    ["Bolt Doe", "Dashing Doe", "Red Hot Doe", "Speedy Doe"],
+    "Dashing Doe",
+  ),
+  Question(
+    "What was your favorite time of year growing up? Why?",
+    ["Late Summer", "Rainy Fall", "Tropic Spring", "Snowy Winters"],
+    "Tropic Spring",
+  ),
+  Question(
+    "Did you play sports as a kid? Which ones?",
+    ["Soccer", "Swimming", "Chess", "Bodybuilding"],
+    "Bodybuilding",
+  ),
+  Question(
+    "What was the most historic event you witnessed as a young person?",
+    [
+      "Moon Landing",
+      "Black Hole image",
+      "DNA modifications",
+      "Launch of google"
+    ],
+    "Moon Landing",
+  ),
+  Question(
+    "What is your favorite cuisine and how has it changed over the years?",
+    ["Chinese", "Indian", "Thai", "Soul Food"],
+    "Indian",
+  ),
+  Question(
+      "Which childhood possession do you still own?",
+      [
+        "Watch from 1956",
+        "Classic Vinyl Records",
+        "Baseball Trading Cards",
+        "Graduation Robes"
+      ],
+      "Graduation Robes"),
+  Question(
+      "What was the first vehicle you owned? ",
+      ["Dodge Charger", "Audi S class", "Rolls Royce", "Range Rover"],
+      "Dodge Charger")
+];
