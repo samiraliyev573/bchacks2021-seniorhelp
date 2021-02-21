cf58648b46487b554cc53bf933717784b4011859
samiraliyev573 <aliyev7137@gmail.com>
2021-02-21 03:10:11 -0800

    Update question.dart
    Co-Authored-By: Prachi Gandhi <78549313+prachigandhi001@users.noreply.github.com>
    Co-Authored-By: Amaan Ali Khan <78450861+AmaanAliK@users.noreply.github.com>

--- seniorhelp/lib/model/question.dart
+++ seniorhelp/lib/model/question.dart
@@ -2,66 +2,68 @@ class Question {
   String question;
   List<String> options;
   String answer;
-  Question(this.question, this.options, this.answer);
+  Question({this.question, this.options, this.answer});
 }
 
 final quiz = [
   Question(
-    "What are your children’s names?",
-    [
+    question: "What are your children’s names?",
+    options: [
       "Jack and Jill Doe",
       "Jayden and Jane Doe",
       "James and Jen Doe) ",
       "Joe and June Doe"
     ],
-    "Jack and Jill Doe",
+    answer: "Jack and Jill Doe",
   ),
   Question(
-    "How old are your children?",
-    ["39 and 36", "45 and 40", "34 and 39", "44 and 55"],
-    "39 and 36",
+    question: "How old are your children?",
+    options: ["39 and 36", "45 and 40", "34 and 39", "44 and 55"],
+    answer: "39 and 36",
   ),
   Question(
-    "What was your nickname growing up?",
-    ["Bolt Doe", "Dashing Doe", "Red Hot Doe", "Speedy Doe"],
-    "Dashing Doe",
+    question: "What was your nickname growing up?",
+    options: ["Bolt Doe", "Dashing Doe", "Red Hot Doe", "Speedy Doe"],
+    answer: "Dashing Doe",
   ),
   Question(
-    "What was your favorite time of year growing up? Why?",
-    ["Late Summer", "Rainy Fall", "Tropic Spring", "Snowy Winters"],
-    "Tropic Spring",
+    question: "What was your favorite time of year growing up? Why?",
+    options: ["Late Summer", "Rainy Fall", "Tropic Spring", "Snowy Winters"],
+    answer: "Tropic Spring",
   ),
   Question(
-    "Did you play sports as a kid? Which ones?",
-    ["Soccer", "Swimming", "Chess", "Bodybuilding"],
-    "Bodybuilding",
+    question: "Did you play sports as a kid? Which ones?",
+    options: ["Soccer", "Swimming", "Chess", "Bodybuilding"],
+    answer: "Bodybuilding",
   ),
   Question(
-    "What was the most historic event you witnessed as a young person?",
-    [
+    question:
+        "What was the most historic event you witnessed as a young person?",
+    options: [
       "Moon Landing",
       "Black Hole image",
       "DNA modifications",
       "Launch of google"
     ],
-    "Moon Landing",
+    answer: "Moon Landing",
   ),
   Question(
-    "What is your favorite cuisine and how has it changed over the years?",
-    ["Chinese", "Indian", "Thai", "Soul Food"],
-    "Indian",
+    question:
+        "What is your favorite cuisine and how has it changed over the years?",
+    options: ["Chinese", "Indian", "Thai", "Soul Food"],
+    answer: "Indian",
   ),
   Question(
-      "Which childhood possession do you still own?",
-      [
+      question: "Which childhood possession do you still own?",
+      options: [
         "Watch from 1956",
         "Classic Vinyl Records",
         "Baseball Trading Cards",
         "Graduation Robes"
       ],
-      "Graduation Robes"),
+      answer: "Graduation Robes"),
   Question(
-      "What was the first vehicle you owned? ",
-      ["Dodge Charger", "Audi S class", "Rolls Royce", "Range Rover"],
-      "Dodge Charger")
+      question: "What was the first vehicle you owned? ",
+      options: ["Dodge Charger", "Audi S class", "Rolls Royce", "Range Rover"],
+      answer: "Dodge Charger")
 ];
